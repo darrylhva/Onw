a53c082ce1ee49f2e390e06e4bcbbc64d7e1c1ff
darrylhva <34041022+darrylhva@users.noreply.github.com>
2018-06-02 16:54:41 +0200

    Add files via upload

--- ImporterenNaarKibana.R
+++ ImporterenNaarKibana.R
@@ -1,34 +1,34 @@
 library(elastic)
 library(rjson)
 
-connect(es_host = "localhost", es_port = 9200,
-        es_transport_schema = "http", errors = "simple")
+connect(es_host = "10.0.2.17", es_port = 9200,
+        es_transport_schema = "http", es_user = "elastic", es_pwd = "zYk7LV0oe07xhvy4DsoT",
+        errors = "simple")
 
 index_delete(index = "poa5")
 index_create(index = "poa5")
-# mapping_get('poa4')
 
 mappingPoa <- '{
-  "poa5": {
+"poa5": {
 "properties": {
-"course_over_ground": { "type": "float" },
-"draught": {"type": "object"},
-"fm_type": {"type": "text", "fields": { "keyword": {"type": "keyword", "ignore_above": 256}}},
+"course_over_ground": { "type": "keyword", "null_value": "NULL" },
+"draught": {"type": "keyword", "null_value": "NULL"},
+"fm_type": {"type": "text", "fields": { "keyword": {"type": "keyword"}}},
 "latitude": {"type": "float" },
-"length": {"type": "long"},
+"length": {"type": "keyword", "null_value": "NULL"},
 "longitude": {"type": "float"},
 "location": {"type": "geo_point"},
-"nav_status": {"type": "text", "fields": {"keyword": {"type": "keyword","ignore_above": 256}}},
+"nav_status": {"type": "keyword", "null_value": "NULL"},
 "received": {"type": "date"},
-"speed_over_ground": {"type": "float"},
-"true_heading": {"type": "object"},
+"speed_over_ground": {"type": "keyword", "null_value": "NULL"},
+"true_heading": {"type": "keyword", "null_value": "NULL"},
 "vessel_id": {"type": "long"},
-"width": {"type": "long"}
+"width": {"type": "keyword", "null_value": "NULL"}
 }}}'
 
 mapping_create(index = "poa5", type="poa5", body=mappingPoa)
 
-setwd("F:/PortAmsterdamData/alles/")
+setwd("C:/DATA/")
 f = list.files(pattern = "*.*", full.names = TRUE)
 
 for(j in f) {
@@ -39,19 +39,18 @@ for(j in f) {
   
   for (i in 1:length(json_data)) {
     location = ""
-    json_data[[i]][["course_over_ground"]] <- NULL
-    json_data[[i]][["speed_over_ground"]] <- NULL
-    json_data[[i]][["draught"]] <- NULL  
-    json_data[[i]][["true_heading"]] <- NULL
-    json_data[[i]][["width"]] <- NULL
-    json_data[[i]][["length"]] <- NULL
-    json_data[[i]][["nav_status"]] <- NULL
+    if(json_data[[i]]["speed_over_ground"] == 'NULL'){ json_data[[i]]["speed_over_ground"] <- 'null' }
+    if(json_data[[i]]["course_over_ground"] == 'NULL'){ json_data[[i]]["course_over_ground"] <- 'null' }
+    if(json_data[[i]]["draught"] == 'NULL'){ json_data[[i]]["draught"] <- 'null' }
+    if(json_data[[i]]["width"] == 'NULL'){ json_data[[i]]["width"] <- 'null' }
+    if(json_data[[i]]["length"] == 'NULL'){ json_data[[i]]["length"] <- 'null' }
+    if(json_data[[i]]["true_heading"] == 'NULL'){ json_data[[i]]["true_heading"] <- 'null' }
+    if(json_data[[i]]["nav_status"] == 'NULL'){ json_data[[i]]["nav_status"] <- 'null' }
     longitude <- json_data[[i]][["longitude"]]
     latitude <- json_data[[i]][["latitude"]]
     location <- paste(latitude, ", ",longitude, sep="")
     json_data[[i]][["location"]] <- location
   }
-  capture.output(docs_bulk(json_data, index="poa5", es_ids = TRUE), file="output.txt")
+  ## voor debug gebruik: capture.output(docs_bulk(json_data, index="poa5", es_ids = TRUE), file="output.txt", append = TRUE)
+  docs_bulk(json_data, index="poa5", es_ids = TRUE)
 }
-
-print("Klaar!!")
